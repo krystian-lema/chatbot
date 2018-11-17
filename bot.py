5de48b10e21b55c998b1ac6ad1f48accf6d8dbe0
Krystian Lema <krystian.lema@gmail.com>
2018-11-17 14:09:47 +0100

    Add train file, improved string formating for bot answers.

--- bot.py
+++ bot.py
@@ -3,21 +3,28 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot import ChatBot
 import os
 
+# Const strings
+bot_display_name = 'Serwis sklepu internetowego'
+user_display_name = 'Użytkownik'
+bot_low_confidence_answer = 'Przepraszam ale nie jestem w stanie odpowiedzieć na to pytanie.'
+bot_greetings = 'Witam! Jak mogę pomóc?'
+bot_bye_answer = 'Do widzenia. Dziękujemy za skorzystanie z naszych usług i zapraszamy ponownie!'
+bye_words = ['do widzenia', 'żegnam', 'zegnam', 'koniec']
+
 # Create chatbot, initialize logic adapters and filters
 bot = ChatBot(
         'Chatbot',
         logic_adapters=[
             "chatterbot.logic.MathematicalEvaluation",
-            "chatterbot.logic.TimeLogicAdapter",
             {
                 "import_path": "chatterbot.logic.BestMatch",
                 "statement_comparision_function": "chatterbot.comparision.levenstein_distance",
-                "response_selection_method": "chatterbot.response_selection.get_first_response"
+                "response_selection_method": "chatterbot.response_selection.get_most_frequent_response"
             },
             {
                 'import_path': 'chatterbot.logic.LowConfidenceAdapter',
                 'threshold': 0.40,
-                'default_response': 'I am sorry, but I do not understand.'
+                'default_response': bot_low_confidence_answer
             }
         ],
         filters=["chatterbot.filters.RepetitiveResponseFilter"]
@@ -26,34 +33,32 @@ bot = ChatBot(
 # Train bot from files
 bot.set_trainer(ListTrainer)
 for _file in os.listdir('train_files'):
-    chats = open('files/' + _file, 'r').readlines()
+    chats = open('train_files/' + _file, 'r').readlines()
     bot.train(chats)
 
 # Train bot with corpus
-bot.set_trainer(ChatterBotCorpusTrainer)
-bot.train("chatterbot.corpus.english.greetings") # test train from coprups english.greetings
+# bot.set_trainer(ChatterBotCorpusTrainer)
+# bot.train("chatterbot.corpus.english.greetings") # test train from coprups english.greetings
 
-# Words to end up a conversation
-bye_words = ['bye', 'Bye']
 continue_dialogue = True
 
 # Start of the conversation
-print('Bot: Hello! How can I help you?')
+print('{0}: {1}'.format(bot_display_name, bot_greetings))
 # Converastion loop
 while continue_dialogue:
 
     # Get input from user
-    request = input('You: ')
+    request = input('{0}: '.format(user_display_name))
 
     # Check if conversation ends or continues
     continue_dialogue = True
     for bye_word in bye_words:
-        if bye_word in request:
+        if bye_word in request.lower():
             continue_dialogue = False
 
     # Display chatbot response
     if continue_dialogue:
         response = bot.get_response(request)
-        print('Bot: {0} ({1} confidence)'.format(response, response.confidence))
+        print('{0}: {1} ({2})'.format(bot_display_name, response, response.confidence))
     else:
-        print('Bot: Bye bye. It was pleasure to talk with you.')
+        print('{0}: {1}'.format(bot_display_name, bot_bye_answer))
